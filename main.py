3e95e3d5877184bf1c901cfded04ed38a1e8fd36
Holliant <85192503+Holliant@users.noreply.github.com>
2022-05-17 01:20:04 +0100

    upload of the game

---
+++ main.py
@@ -0,0 +1,44 @@
+#Welcomes user to the game
+print("Welcome da dice game")
+
+import random
+result = 0
+
+#roles the 2 dice pregame
+st = random.randint(1,6)
+nd = random.randint(1,6)
+
+#roles and displays the first role
+print("|-------------------|")
+print("| Rolling your dice |")
+print("|-------------------|")
+print("The value of the first dice is " + str(st) +"")
+
+#roles and displays the second role
+print("|-------------------|")
+print("| Rolling your dice |")
+print("|-------------------|")
+print("The value of the second dice is " + str(nd) +"")  
+
+#add the scores if any of the var = 1 or 6  
+if st == 1:
+   result = result + 50
+   
+if nd == 1:
+   result = result + 50
+
+if st == 6:
+   result = result + 50
+   
+if nd == 6:
+   result = result + 50
+
+    
+
+
+if result == int(100):
+    print("Congrdulations your score is " + str(result) + " you win!!!")
+else:
+    print("game repating is wip")
+    print(" Your score is " + str(result) + " though")
+
