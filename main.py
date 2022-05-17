f84b77224f477a938720cc699c3f72024aa08cb7
Holliant <85192503+Holliant@users.noreply.github.com>
2022-05-17 18:18:57 +0100

    finished product

--- main.py
+++ main.py
@@ -2,35 +2,48 @@
 print("Welcome da dice game")
 
 import random
-result = 0
 
-#roles the 2 dice pregame
-st = random.randint(1,6)
-nd = random.randint(1,6)
+def start_game():
+   life = 3
 
-#roles and displays the first role
-print("|-------------------|")
-print("| Rolling your dice |")
-print("|-------------------|")
-print("The value of the first dice is " + str(st) +"")
+   while life > 0:
 
-#roles and displays the second role
-print("|-------------------|")
-print("| Rolling your dice |")
-print("|-------------------|")
-print("The value of the second dice is " + str(nd) +"")  
+      result = 0
 
-#add the scores if any of the var = 1 or 6  
-if st in [1,6]:
-   result = result + 50
-   
-if nd in [1,6]:
-   result = result + 50
+      #roles the 2 dice pregame
+      st = random.randint(1,6)
+      nd = random.randint(1,6)
 
-#end txt for the game
-if result == int(100):
-    print("Congrdulations your score is " + str(result) + " you win!!!")
-else:
-    print("game repating is wip")
-    print(" Your score is " + str(result) + " though")
+      #roles and displays the first role
+      # print("|-------------------|")
+      # print("| Rolling your dice |")
+      # print("|-------------------|")
+      print("The value of the first dice is " + str(st) +"")
 
+      #roles and displays the second role
+      # print("|-------------------|")
+      # print("| Rolling your dice |")
+      # print("|-------------------|")
+      print("The value of the second dice is " + str(nd) +"")  
+
+      print("")
+
+      #add the scores if any of the var = 1 or 6  
+      if st in [1,6]:
+         result = result + 50
+         
+      if nd in [1,6]:
+         result = result + 50
+
+      life = life - 1
+
+   #end txt for the game
+   if result == int(100):
+      print("Congratulations your score is " + str(result) + " you win!!!")
+   else:
+      print("You lost...")
+
+while True:
+   start_game()
+   if input("Do you want to play again? (Y/N)") == "N":
+      exit()
