3f002e0ca165d112b6f02ea6eef8b882a802fe08
Holliant <85192503+Holliant@users.noreply.github.com>
2022-05-17 12:05:30 +0100

    made the scoring  system with lists instead of 1 by 1

--- main.py
+++ main.py
@@ -21,21 +21,13 @@ print("|-------------------|")
 print("The value of the second dice is " + str(nd) +"")  
 
 #add the scores if any of the var = 1 or 6  
-if st == 1:
+if st in [1,6]:
    result = result + 50
    
-if nd == 1:
+if nd in [1,6]:
    result = result + 50
 
-if st == 6:
-   result = result + 50
-   
-if nd == 6:
-   result = result + 50
-
-    
-
-
+#end txt for the game
 if result == int(100):
     print("Congrdulations your score is " + str(result) + " you win!!!")
 else:
